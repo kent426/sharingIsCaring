645e93b9f9d9a6e9a50bdbcfd958ee52aaa6c2f7
Eric <elin03277@gmail.com>
2017-04-13 02:39:25 -0700

    temp result page

---
+++ result.php
@@ -0,0 +1,50 @@
+<html>
+    <head>
+        <style>
+            h1 {
+                font-size: 80px;
+            }
+
+            h2 {
+                text-align: center;
+            }
+
+            #pic {
+                align-items: center;
+                height: 300px;
+                width: 600px;
+            }
+
+            head, body {
+                background-color: lightblue;
+            }
+        </style>
+    </head>
+
+    <body>
+        <div id="fb-root"></div>
+        <script>(function(d, s, id) {
+                var js, fjs = d.getElementsByTagName(s)[0];
+                if (d.getElementById(id)) return;
+                js = d.createElement(s); js.id = id;
+                js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.8";
+                fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));</script>
+
+        <div style="display: table; margin: 0 auto">
+
+            <h2 id="h2">The character you are most like is:</h2>
+
+            <h1 id="h1">Kirigaya Kazuto</h1>
+
+            <img src="kirito.png" id="pic">
+        </div>
+
+        <div style="display: table; margin: 0 auto; padding-top: 10px">
+
+            <div class="fb-share-button" data-href="http://localhost:8887/sharingIsCaring/result.php" data-layout="button_count" data-size="small" data-mobile-iframe="true"><a class="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Flocalhost%3A8887%2FsharingIsCaring%2Fresult.php&amp;src=sdkpreparse">Share</a></div>
+
+        </div>
+
+    </body>
+</html>
