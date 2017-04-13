70f1c9b9f0de863ae5b82e6c85ec997a5c3c6045
Gabriel Yip <gyip16@my.bcit.ca>
2017-04-13 05:03:41 -0700

    Adjustments
    Handles survey to result page
    Can share on live server
    handles dynamic json files

--- survey.php
+++ survey.php
@@ -2,21 +2,32 @@
 
     <?php
     //parse the json file
-    $json = file_get_contents("./he.json");
-    $j = json_decode(stripslashes($json));
+    if(isset($_GET['form'])) {
+        if($json = file_get_contents('http://projectv.me/api/'.$_GET['form'].'.json')) {
+            $j = json_decode(stripslashes($json));
+            foreach ($j->pages[0]->elements as $onequestion) {
+                echo "<fieldset>";
+                //one question with choices
+                echo "<legend>$onequestion->name</legend>";
+                foreach ($onequestion->choices as $oneitem) {
+                    //one choice
+                    echo "<input type='radio' name=$onequestion->name value=$oneitem->value />$oneitem->text<br />";
+                }
+                echo "</fieldset>";
+                echo "<br/>";
+            }
+            echo "<input type='submit' value='Submit now' />";
+        } else {
+            echo "I don't know";
+        }
+    } else {
+        echo "I don't know";
+    }
+
+
     //array of questions
 
-    foreach( $j->pages[0]->elements as $onequestion) {
-        echo "<fieldset>";
-        //one question with choices
-        echo "<legend>$onequestion->name</legend>";
-        foreach ($onequestion->choices as $oneitem) {
-            //one choice
-            echo "<input type='radio' name=$oneitem->text value=$oneitem->text />$oneitem->text<br />";
-        }
-        echo "</fieldset>";
-    }
-    echo "<input type='submit' value='Submit now' />";
+
     ?>
 </form>
 
